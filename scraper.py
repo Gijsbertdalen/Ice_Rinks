0cd67f02c6edd7e774682192a2a556f540e5c1c1
Paul Bradshaw <paulonhismobile@gmail.com>
2017-03-13 07:57:50 +0000

    copy code from...
    https://github.com/paulbradshaw/scrapers/blob/master/iceRinks.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,45 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
+import urllib
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+#scrape table function
+def scrape_table(root):
+    rows = root.cssselect("table#AutoNumber2 tr")  # selects all <tr blocks in <table id="AutoNumber2"
+    for row in rows[0:-2]:
+        # Set up our data record - we'll need it later
+        record = {}
+        table_cells = row.cssselect("td")
+        if table_cells: 
+            record['Rink'] = table_cells[0].text_content()
+            record['Address'] = table_cells[1].text_content()
+            print "len", len(table_cells[2].text_content())
+            if len(table_cells[2].text_content())>0:
+                record['Area'] = table_cells[2].text_content()
+            record['County'] = table_cells[3].text_content()
+            record['Phone'] = table_cells[4].text_content()
+            showteamsurls = table_cells[5].cssselect('a')
+            mapurls = table_cells[6].cssselect('a')
+            if len(mapurls)>0:
+                record['Maplink'] = mapurls[0].attrib.get('href')
+            else:
+                record['Maplink'] = "NO MAP LINK"
+            if len(showteamsurls)>0:
+                record['teamslink'] = showteamsurls[0].attrib.get('href')
+            else:
+                record['teamslink'] = "NO MAP LINK"
+            # Print out the data we've gathered
+            print record, '------------'
+            # Finally, save the record to the datastore - 'Artist' is our unique key
+            scraperwiki.sql.save(["Rink"], record)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+
+base_url = 'http://www.eiharec.co.uk/rink_list.php'
+starting_url = base_url
+def scrape_and_look_for_next_link(url):
+    html = scraperwiki.scrape(url)
+    #print html
+    root = lxml.html.fromstring(html)
+    scrape_table(root)
+    
+
+scrape_and_look_for_next_link(starting_url)
